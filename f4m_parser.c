27458118a506d2f576be095582edbf9a3f91b89a
Yunfan Ye <yunfanye@cmu.edu>
2015-11-22 23:10:56 -0500

    parse f4m xml document

---
+++ f4m_parser.c
@@ -0,0 +1,17 @@
+#define _GNU_SOURCE
+
+#include <string.h>
+#include "f4m_parser.h"
+
+/* extract bitrate value from
+ * <media url="/myvideo/high" bitrate="1708" width="1920" height="1080"/> 
+ * return malloc-ed bitrate list
+ */
+unsigned * extract_bitrate_list(char * buf, int size) {
+	
+	buf[size] = '\0';
+	
+	while(strcasestr()) {
+	
+	}
+}
---
+++ f4m_parser.h
@@ -0,0 +1,6 @@
+#ifndef _F4M_PARSER_H_
+#define _F4M_PARSER_H_
+
+unsigned * extract_bitrate_list(char * buf, int size);
+
+#endif
